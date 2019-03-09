ccb4a0080202c070dbe84600af3ecd65d112c213
Estela Balboa <estela.balboa@gmail.com>
2019-03-09 10:08:00 +0100

    feat: Añadidas representaciones de las correlaciones entre variables

--- seedClassifier.py
+++ seedClassifier.py
@@ -4,9 +4,13 @@ import pandas as pd
 from time import time
 from IPython.display import display
 import matplotlib.pyplot as plt
+from jedi.refactoring import inline
+from matplotlib import *
+import scipy
 
 #%matplotlib inline
-
+import os
+os.chdir("/Users/ebalboa/Documents/AI-SATURDAYS/20190223/support-vector-machine-seed-classifier/")
 
 class SeedClassifier():
 
@@ -15,29 +19,38 @@ class SeedClassifier():
         df = pd.read_csv("data/seeds.csv")
 
         # Mostrar número de filas y columnas del dataframe
-        print("Filas: " + str(df.shape[0]) + " Columnass: " + str(df.shape[1]))
+        print("Filas: " + str(df.shape[0]) + " Columnas: " + str(df.shape[1]))
 
-        # Mostrar las primeras 30 filas
-        display(df.head(30))
+        # Mostrar las primeras 10 filas
+        display(df.head(10))
 
         # Hallar número de valores únicos en cada columna
-        display("Valores únicos por columnas: " + '\n' + str(df.nunique()))
+        print("Valores únicos por columnas: " + '\n' + str(df.nunique()))
 
         # Comprobar la existencia de valores nulos en el dataset
-        display("Nulos por columnas: " + '\n' + str(df.isnull().any()))
+        print("Nulos por columnas: " + '\n' + str(df.isnull().any()))
 
         # Mostrar información general del dataframe
-        display(df.info())
+        print(df.info())
 
         # Descripción analítica básica del dataframe
-        display(df.describe())
+        print(np.round(df.describe()))
 
         # Mostrar matriz de correlación de variables
         # Pista: explore plt.matshow y corr() de un dataframe
 
+        #for col in df[0]:
+                # Check the correlation of a pair of columns
+        corr_val = np.round(df.corr(),4)
+        # Logic to compare corr_max with current corr_val
+        print(corr_val)
+
+        # Esta funcion en el IDE no se representa, si desde la terminal
+        plt.matshow(corr_val)
 
         # Mostrar correlaciones como una función discreta entre las diferentes variables con una matriz
         # útil para apreciar relaciones lineales
+        pd.plotting.scatter_matrix(df, figsize=(40, 40), diagonal='kde')
 
         # Pista: explore pd.plotting.scatter_matrix
 
